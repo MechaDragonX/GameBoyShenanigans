0d683ae527f8f7937f0293872ece9edba3bcfcad
MechaDragonX <theofficialrv@gmail.com>
2020-04-04 11:35:55 -0700

    Change make files in the other projects ot match BG

--- HelloWorld/make.bat
+++ HelloWorld/make.bat
@@ -1,2 +1,3 @@
-c:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -c -o out\main.o main.c
+del /S out\*
+c:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -c -o out\main.o src\Main.c
 c:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -o out\main.gb out\main.o
